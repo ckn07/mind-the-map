85562a06efa655b202ec6eb965eeda1f7be38f32
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-03 19:00:37 +0100

    validates POI avec commentaires

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,2 +1,15 @@
 class Poi < ApplicationRecord
+  has_many :anwsers
+  has_many :theme_pois
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+
+  # a deccommente quand on aura sette carrier / cloudinary
+  # validates :photo, presence: true
+  # mount_uploader :photo, PhotoUploader
+
+  # a decommente quand on aura mis en place le geocoder
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 end
