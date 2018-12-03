90c97fcfec3b4eff070c26110352a8d95b16ca7c
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-03 18:48:50 +0100

    validates city

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,7 @@
 class City < ApplicationRecord
+  has_many :themes
+  validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
+  # a deccommente quand on aura sette carrier / cloudinary
+  # mount_uploader :photo, PhotoUploader
 end
