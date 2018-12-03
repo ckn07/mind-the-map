048c27b733e181db42fa2382646c2ea5b40c8f2a
Cecile Dezy <cecile.dezy@gmail.com>
2018-12-03 20:28:31 +0100

    added some routes, to be continued

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'answers/show'
   get 'answers/index'
   get 'users/show'
@@ -9,7 +10,29 @@ Rails.application.routes.draw do
   get 'cities/show'
   get 'cities/index'
   get 'games/show'
+
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+#   resources :users, only: [:show, :index, :edit, :update]
+#   resources :cities, only: [:index, :show] do
+#     resources :themes, only: [:index, :show] do
+#       resources :theme_pois, only: [:index, :show]
+#     end
+#     resources :pois, only: [:index, :show] do
+#       resources :theme_pois, only: [:index, :show]
+#     end
+#   end
+
+#   namespace :admin do
+#     resources :themes, only: [:new, :create, :edit, :update, :delete]
+#     resources :users
+#   end
+
+# end
+
+#   add_foreign_key "answers", "games"
+#   add_foreign_key "answers", "pois"
+#   add_foreign_key "answers", "users"
+#   add_foreign_key "games", "themes"
+#   add_foreign_key "theme_pois", "pois", column: "pois_id"
