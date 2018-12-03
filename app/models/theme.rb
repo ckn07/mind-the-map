4495f7cffdf5d43d0c48ab5c59f48b717d976f7f
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-03 18:57:21 +0100

    validates themes

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,3 +1,9 @@
 class Theme < ApplicationRecord
   belongs_to :city
+  has_many :theme_pois
+  has_many :games
+  validates :name, presence: true
+  # a deccommente quand on aura sette carrier / cloudinary
+  # validates :photo, presence: true
+  # mount_uploader :photo, PhotoUploader
 end
