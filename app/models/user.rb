c29661a680b46d23409f3160c9ca657b4fc48931
Benoit Delest <benoitdelest@gmail.com>
2018-12-04 18:06:15 +0100

    seed ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,6 @@ class User < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 end
