729ec1e01e3bbab9554ff24c7b2eaae3d31c5b00
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-04 15:14:39 +0100

    setup devise, suppression de 2 fichier de migration non necessaire

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,6 @@ class User < ApplicationRecord
   # a decommente quand on aura installe le geocoder
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
+   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- db/migrate/20181204111157_add_coordinates_to_pois.rb
+++
@@ -1,6 +0,0 @@
-class AddCoordinatesToPois < ActiveRecord::Migration[5.2]
-  def change
-    add_column :pois, :latitude, :float
-    add_column :pois, :longitude, :float
-  end
-end
