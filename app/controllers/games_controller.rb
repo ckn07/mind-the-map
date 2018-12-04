11defc820564e118ba4730d3234d299ea9df3256
Cecile Dezy <cecile.dezy@gmail.com>
2018-12-04 18:52:11 +0100

    added mapbox key and add show method on games ctrlr

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,12 @@
 class GamesController < ApplicationController
+
+  # affichage de la page finale du jeu
   def show
+    @user = current_user
+    @game = Game.find(params[:id])
   end
+
+  def create
+  end
+
 end
