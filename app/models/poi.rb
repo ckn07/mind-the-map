133cc357033cbc9c2f093db6c2fabb8652bf5544
Christophe NOUJAIM <christophe@noujaim.com>
2018-12-04 18:01:27 +0100

    uncommented validations

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,10 +1,10 @@
 class Poi < ApplicationRecord
   has_many :anwsers
   has_many :theme_pois
-  # validates :name, presence: true
-  # validates :address, presence: true
-  # validates :description, presence: true
-  # validates :photo, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
 
   geocoded_by :address
   mount_uploader :photo, PhotoUploader
