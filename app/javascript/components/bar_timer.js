d687b7de2159613959ec012729e24edce7a532be
Cecile Dezy <cecile.dezy@gmail.com>
2018-12-06 13:00:30 +0100

    timer bar

---
+++ app/javascript/components/bar_timer.js
@@ -0,0 +1,26 @@
+var $bar, perc, start, update;
+
+$bar = $('.percentage');
+
+perc = 0;
+
+update = function() {
+  $bar.css("height", perc + '%');
+  $bar.attr("perc", Math.floor(perc) + '%');
+  if (perc < 100) {
+    perc += 0.1;
+    if (Math.floor(perc) === 5) {
+      $bar.addClass('active');
+    }
+    if (Math.floor(perc) === 95) {
+      return $bar.removeClass('active');
+    }
+  }
+};
+
+startTimer = function() {
+  if ($('.bar').length > 0);
+  setInterval(update, 10);
+};
+
+startTimer();
