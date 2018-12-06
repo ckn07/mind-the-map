42252def503125a4e4c56385451bd1cfc3347178
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-06 15:51:03 +0100

    controller create + ajout des liens pour démarrer un jeux au click sur les cartes de theme index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,6 +9,31 @@ class GamesController < ApplicationController
   end
 
   def create
+    @user = current_user
+    # instanciation par défaut sur Paris (city id 1)
+    @game = Game.new
+    @game.user_one = @user
+    @theme = Theme.find(params[:theme_id])
+    @game.theme = @theme
+    @poi = pick_a_random_poi_of_the_them
+    if @game.save
+      redirect_to game_poi_path(@game,@poi)
+    else
+      raise
+      redirect_to root_path
+    end
+
   end
+
+  private
+  def pick_a_random_poi_of_the_them
+    @pois = @theme.theme_pois
+    @list_poi_on_going_game = []
+    @pois.each do |poi|
+      @list_poi_on_going_game << (poi.poi_id)
+    end
+    first_poi = @list_poi_on_going_game.sample
+  end
+
 
 end
