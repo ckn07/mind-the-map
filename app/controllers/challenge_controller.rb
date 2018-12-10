900b740204159e3c3cba3cda9477202654b90fcd
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-10 18:18:25 +0100

    merde putain j'avais pas mis de S à mon controller et mes views challenges

--- app/controllers/challenge_controller.rb
+++
@@ -1,7 +0,0 @@
-class ChallengeController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-end
---
+++ app/controllers/challenges_controller.rb
@@ -0,0 +1,27 @@
+class ChallengeController < ApplicationController
+  def new
+    @game = Game.find(params[:id])
+  end
+
+  def create
+  @game = Game.find(params[:id])
+  @game.user_two = current_user
+  @poi = pick_a_random_poi_of_the_theme
+    if @game.save
+      redirect_to game_poi_path(@game,@poi)
+    else
+      redirect_to root_path
+    end
+  end
+
+private
+  def pick_a_random_poi_of_the_theme
+    @pois = @theme.theme_pois
+    @list_poi_on_going_game = []
+    @pois.each do |poi|
+      @list_poi_on_going_game << (poi.poi_id)
+    end
+    first_poi = @list_poi_on_going_game.sample
+  end
+
+end
