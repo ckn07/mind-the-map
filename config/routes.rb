e5d98588af716ee3ec8d2e985f40faad7e55d565
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-10 17:45:47 +0100

    Update routes.rb
    la route existait en double

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'challenge/new'
-  get 'challenge/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
