6d31dc9bd3103452fdb307a5bb753200e1884c40
Jeremy Jouffroy <jeremy.jouffroy@gmail.com>
2018-12-10 17:00:00 +0100

    initialisation du controller challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'challenge/new'
+  get 'challenge/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
