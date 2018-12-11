baf179fcb8803e3c3fba5233acd1f6127802e144
Christophe NOUJAIM <christophe@noujaim.com>
2018-12-11 18:41:51 +0100

    leaderboard highscore duo done tx Cecile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,13 +10,26 @@ class PagesController < ApplicationController
   def leaderboard
     @cities = City.all
     @themes = Theme.all
+
     # workaround of when the page loads, there is no option selected
-    if params[:query].present?
-      @theme = Theme.find_by(name: params[:theme])
-      @games_unsorted = @theme.games
-      @games = @games_unsorted.sort_by(&:score_one).reverse
+    if params[:theme].present?
+      # @city = City.find(params[:city])
+      # @theme = Theme.find(params[:theme])
+      # @games_unsorted = @theme.games
+      @games = Game.where(theme_id: params[:theme])
     else
       @games = Game.all
     end
+
+    @games = @games.
+      select("
+          games.*,
+          CASE WHEN score_two IS NULL THEN score_one
+               WHEN score_one >= score_two THEN score_one
+               ELSE score_two
+          END
+          AS score
+        ").
+      order("score DESC")
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,12 @@ class Game < ApplicationRecord
   belongs_to :user_one, class_name: 'User'
   belongs_to :user_two, class_name: 'User', optional: true
   has_many :answers, dependent: :destroy
+
+  def winner
+    if score_two && score_two > score_one
+      user_two
+    else
+      user_one
+    end
+  end
 end
