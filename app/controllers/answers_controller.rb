ac73099aea771f6bde7914177bb5ae333b26c9ed
Benoit Delest <benoitdelest@gmail.com>
2018-12-12 19:05:46 +0100

    wallah à la bien  avec jéjé bro

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -92,7 +92,7 @@ class AnswersController < ApplicationController
       @list_poi_on_going_game << poi.poi_id
     end
     @pois_already_answered = []
-    @list_answers = Answer.where(game_id: @game)
+    @list_answers = Answer.where(game_id: @game, user: current_user)
     @list_answers.each do |poi|
       @pois_already_answered << poi.poi_id
     end
