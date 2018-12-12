0978151f6088054cc96ec30d5b715cbefc00ceb2
Benoit Delest <benoitdelest@gmail.com>
2018-12-12 09:26:26 +0100

    update user_controller if not game

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @games = @user.games
-    @games = @games.sort_by { |game| game.score_one || game.score_two }
+    @games = @games.sort_by { |game| game.score_one || game.score_two
+    if game.score_one && game.score_two == nil
+      return "Go play"
+    elsif game.score_one == nil && game.score_two != nil
+      return game.score_two
+    else
+      return game.score_one
+    end }
     @games.uniq! { |game| game.theme }
   end
 
